85c3d38418495d221c311d0d45473bf9cf3882da
lookymlive <lookymlive@gmail.com>
2025-02-13 19:28:44 -0300

    add prettier integration and update eslint configuration

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View} from "react-native";
+import { StyleSheet, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { Main } from "./components/Main";
-
 
 export default function App() {
   return (
